153fbd11d6f91d0bd74ac25b44b90e741743c1c4
Rafael Tauschek <rafael.tauschek@gmail.com>
2023-08-07 18:57:54 +0200

    backgroundimages and image animation

--- models/cloud.class.js
+++ models/cloud.class.js
@@ -1,5 +1,4 @@
-class Cloud extends MovableObject {
-    x = Math.random() * 500;
+class Cloud extends MovableObject {  
     y = 20;
     height = 250;
     width = 500;
@@ -7,6 +6,13 @@ class Cloud extends MovableObject {
     constructor() {
         super().loadImage('img/5_background/layers/4_clouds/1.png');
 
+        this.x = Math.random() * 500;
+        this.animate();
+    }
 
+    animate() {
+        setInterval(() => {
+            this.x -= 0.1;
+        }, 1000 / 60);  
     }
 }
