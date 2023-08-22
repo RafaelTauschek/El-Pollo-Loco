3d4a2be0117d914f63f4dd2adc6c47ec4d61abe1
Rafael Tauschek <rafael.tauschek@gmail.com>
2023-08-22 21:26:39 +0200

    reorganized functions

--- models/movable-object.class.js
+++ models/movable-object.class.js
@@ -1,11 +1,4 @@
-class MovableObject {
-    x = 120;
-    y = 280;
-    img;
-    height = 150;
-    width = 100;
-    imageCache = {};
-    currentImage = 0;
+class MovableObject extends DrawableObject {
     speed = 0.10;
     otherDirection = false;
     speedY = 0;
@@ -24,23 +17,6 @@ class MovableObject {
 
     isAboveGround() {
         return this.y < 120;
-    }
-
-    loadImage(path) {
-        this.img = new Image();
-        this.img.src = path;
-    }
-
-    loadImages(arr) {
-        arr.forEach((path) => {
-            let img = new Image();
-            img.src = path;
-            this.imageCache[path] = img;
-        });
-    }
-
-    draw(ctx) {
-        ctx.drawImage(this.img, this.x, this.y, this.width, this.height);
     }
 
     drawFrame(ctx) {
@@ -78,7 +54,6 @@ class MovableObject {
     isDead() {
         return this.energy == 0;
     }
-
 
     playAnimation(images) {
         let i = this.currentImage % images.length;
